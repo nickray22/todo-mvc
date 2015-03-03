ce40e2b29a22e326862e4c184fc2260bfafc7af9
Nick Ray <nickray22@yahoo.com>
2015-03-03 17:18:38 -0500

    remove edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
-  resources :todos, :only => [ :index, :create, :edit, :update, :destroy ]
+  resources :todos, :only => [ :index, :create, :update, :destroy ]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
